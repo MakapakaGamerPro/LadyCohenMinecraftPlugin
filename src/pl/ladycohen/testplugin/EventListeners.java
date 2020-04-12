e5e54b6b2f9177d5e69a700d384f39dd266252f1
piotrojrzynski1 <33787567+piotrojrzynski1@users.noreply.github.com>
2020-04-12 19:48:53 +0200

    Initial commit
    /napisz command, acts like simple msg
    global chat override to read roles from LuckPerms

---
+++ src/pl/ladycohen/testplugin/EventListeners.java
@@ -0,0 +1,67 @@
+package pl.ladycohen.testplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.model.user.UserManager;
+
+public class EventListeners implements Listener {
+	GroupLibrary _groupLibrary = new GroupLibrary();
+	LuckPerms _perms;
+
+	public EventListeners(){
+		Bukkit.getLogger().info("Wczytywanie LuckPerms do LadyCohenPlugin");
+		RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
+		if (provider != null) {
+			Bukkit.getLogger().info("Wczytano dostawcê LuckPerms");
+		    _perms = provider.getProvider();
+		}
+		else{
+			Bukkit.getLogger().severe("Nie za³adowano providera LuckPerms");
+		}
+	}
+	
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent event) {
+    	try {
+	    	LuckyGroup group = getPlayerGroup(event.getPlayer());
+	    	
+	    	if(group!=null) {
+	    		event.setFormat(group.PrefixColor + "[" + group.Name+ "]" + ChatColor.BLUE + "%11s: " + ChatColor.WHITE + " %s");
+	    	}
+    	}
+    	catch(Exception e){
+            event.getPlayer().sendMessage(e.toString());
+    	}
+    }
+    
+    public LuckyGroup getPlayerGroup(Player player) {
+    	UserManager userManager = _perms.getUserManager();
+    	if(userManager==null)
+    		player.sendMessage("nie znaleziono menad¿era grup");
+        
+    	User user = userManager.getUser(player.getUniqueId());
+    	if(user==null)
+            player.sendMessage("nie znaleziono u¿ytkownika " +player.getName());
+    		
+    	String groupName = user.getPrimaryGroup();
+    	if(groupName==null)
+            player.sendMessage("nie znaleziono grupy u¿ytkownika");
+    	    	
+        for (LuckyGroup group : _groupLibrary.GetGroups()) {
+            if (group.Name.equalsIgnoreCase(groupName)) {
+                return group;
+            }
+        }
+        
+        return null;
+    }
+}
---
+++ src/pl/ladycohen/testplugin/GroupLibrary.java
@@ -0,0 +1,19 @@
+package pl.ladycohen.testplugin;
+
+import java.util.ArrayList;
+import org.bukkit.ChatColor;
+
+public class GroupLibrary {
+	
+	public ArrayList<LuckyGroup> GetGroups(){
+    	return new ArrayList<LuckyGroup>() {{
+    	    add(new LuckyGroup("W£AŒCICIEL", ChatColor.UNDERLINE.toString() + ChatColor.MAGIC.toString()));
+    	    add(new LuckyGroup("@-Admin", ChatColor.GREEN.toString()));
+    	    add(new LuckyGroup("Admin", ChatColor.AQUA.toString()));
+    	    add(new LuckyGroup("pomocnik_admina", ChatColor.YELLOW.toString()));
+    	    add(new LuckyGroup("Moderator", ChatColor.DARK_GREEN.toString()));
+    	    add(new LuckyGroup("S-VIP", ChatColor.DARK_RED.toString()));
+    	    add(new LuckyGroup("VIP", ChatColor.GRAY.toString()));
+    	}};
+	}
+}

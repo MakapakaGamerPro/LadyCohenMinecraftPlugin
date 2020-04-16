76bcafb31c4a54513415cb9cec87f950894da9fe
MakapakaGamerPro <63794929+MakapakaGamerPro@users.noreply.github.com>
2020-04-16 15:29:38 +0200

    Update GroupLibrary.java

--- src/pl/ladycohen/testplugin/GroupLibrary.java
+++ src/pl/ladycohen/testplugin/GroupLibrary.java
@@ -7,12 +7,13 @@ public class GroupLibrary {
 	
 	public ArrayList<LuckyGroup> GetGroups(){
     	return new ArrayList<LuckyGroup>() {{
-    	    add(new LuckyGroup("W£AŒCICIEL", ChatColor.UNDERLINE.toString() + ChatColor.MAGIC.toString()));
-    	    add(new LuckyGroup("@-Admin", ChatColor.GREEN.toString()));
+    	    add(new LuckyGroup("WÂ£AÅ’CICIEL", ChatColor.UNDERLINE.toString() + ChatColor.MAGIC.toString()));
     	    add(new LuckyGroup("Admin", ChatColor.AQUA.toString()));
     	    add(new LuckyGroup("pomocnik_admina", ChatColor.YELLOW.toString()));
     	    add(new LuckyGroup("Moderator", ChatColor.DARK_GREEN.toString()));
+	    add(new LuckyGroup("YouTuber", ChatColor.RED.toString()));	
     	    add(new LuckyGroup("S-VIP", ChatColor.DARK_RED.toString()));
+	    add(new LuckyGroup("YouTuber", ChatColor.RED.toString()));
     	    add(new LuckyGroup("VIP", ChatColor.GRAY.toString()));
     	}};
 	}
